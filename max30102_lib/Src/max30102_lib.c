8bba0921a3a3a855815abcad800a0aaa043a39be
LuongHuuPhuc <Huuphuc1016@gmail.com>
2026-06-14 00:50:48 +0700

    Add some comment and change some code for more clear

--- max30102_lib/Src/max30102_lib.c
+++ max30102_lib/Src/max30102_lib.c
@@ -43,38 +43,51 @@ void max30102_reset(max30102_t *obj){
 
 /*-----------------------------------------------------------*/
 
-void max30102_set_a_full(max30102_t *obj, uint8_t enable){
-    uint8_t reg = 0;
+void max30102_set_a_full_itrp(max30102_t *obj, uint8_t enable){
+    uint8_t reg = 0x00;
+
+    /* Doc gia tri thanh ghi INTERRUPT_ENABLE_1 de lay trang thai On-Off hien tai */
     max30102_read(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
-    reg &= ~(0x01 << MAX30102_INTERRUPT_A_FULL);
-    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_A_FULL);
+
+    /* Clear bit */
+    reg &= ~(0x01 << MAX30102_INTERRUPT_A_FULL_EN);
+    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_A_FULL_EN);
+
+    /* Ghi lai gia tri vao thanh ghi */
     max30102_write(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
 }
 
 /*-----------------------------------------------------------*/
 
-void max30102_set_ppg_rdy(max30102_t *obj, uint8_t enable){
+void max30102_set_ppg_rdy_itrp(max30102_t *obj, uint8_t enable){
     uint8_t reg = 0;
     max30102_read(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
-    reg &= ~(0x01 << MAX30102_INTERRUPT_PPG_RDY);
-    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_PPG_RDY);
+    reg &= ~(0x01 << MAX30102_INTERRUPT_PPG_RDY_EN);
+    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_PPG_RDY_EN);
     max30102_write(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
 }
 
 /*-----------------------------------------------------------*/
 
-void max30102_set_alc_ovf(max30102_t *obj, uint8_t enable){
+void max30102_set_alc_ovf_itrp(max30102_t *obj, uint8_t enable){
     uint8_t reg = 0;
     max30102_read(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
-    reg &= ~(0x01 << MAX30102_INTERRUPT_ALC_OVF);
-    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_ALC_OVF);
+    reg &= ~(0x01 << MAX30102_INTERRUPT_ALC_OVF_EN);
+    reg |= ((enable & 0x01) << MAX30102_INTERRUPT_ALC_OVF_EN);
     max30102_write(obj, MAX30102_INTERRUPT_ENABLE_1, &reg, 1);
 }
 
 /*-----------------------------------------------------------*/
 
-void max30102_set_die_temp_rdy(max30102_t *obj, uint8_t enable){
-    uint8_t reg = (enable & 0x01) << MAX30102_INTERRUPT_DIE_TEMP_RDY;
+void max30102_set_power_ready(max30102_t *obj, uint8_t enable){
+	uint8_t reg = (enable & 0x01) << MAX30102_INTERRUPT_PWR_RDY;
+	max30102_write(obj, MAX30102_INTERRUPT_STATUS_1, &reg, 1);
+}
+
+/*-----------------------------------------------------------*/
+
+void max30102_set_die_temp_rdy_itrp(max30102_t *obj, uint8_t enable){
+    uint8_t reg = (enable & 0x01) << MAX30102_INTERRUPT_DIE_TEMP_RDY_EN;
     max30102_write(obj, MAX30102_INTERRUPT_ENABLE_2, &reg, 1);
 }
 
@@ -101,32 +114,31 @@ uint8_t max30102_has_interrupt(max30102_t *obj){
 
 void max30102_interrupt_handler(max30102_t *obj, max30102_record *record){
     uint8_t reg[2] = {0x00};
-    // Interrupt flag in registers 0x00 and 0x01 are cleared on read
+
+    // Bang cach doc thanh ghi Interrupt Status 1-2 (0x00 va 0x01) -> tat ca cac bit se duoc clear/reset
     max30102_read(obj, MAX30102_INTERRUPT_STATUS_1, reg, 2);
 
-    if ((reg[0] >> MAX30102_INTERRUPT_A_FULL) & 0x01)
-    {
-        // FIFO almost full
+    if ((reg[0] >> MAX30102_INTERRUPT_A_FULL) & 0x01){
+        // FIFO gan day thi doc FIFO
     	max30102_read_fifo_on_interrupt(obj, record);
     }
 
-    if ((reg[0] >> MAX30102_INTERRUPT_PPG_RDY) & 0x01)
-    {
+    if ((reg[0] >> MAX30102_INTERRUPT_PPG_RDY) & 0x01){
         // New FIFO data ready
+    	/*...*/
     }
 
-    if ((reg[0] >> MAX30102_INTERRUPT_ALC_OVF) & 0x01)
-    {
+    if ((reg[0] >> MAX30102_INTERRUPT_ALC_OVF) & 0x01){
         // Ambient light overflow
+    	/*...*/
     }
 
-    if ((reg[1] >> MAX30102_INTERRUPT_DIE_TEMP_RDY) & 0x01)
-    {
+    if ((reg[1] >> MAX30102_INTERRUPT_DIE_TEMP_RDY) & 0x01){
         // Temperature data ready
         int8_t temp_int;
         uint8_t temp_frac;
         max30102_read_temp(obj, &temp_int, &temp_frac);
-        // float temp = temp_int + 0.0625f * temp_frac;
+        obj->sensor_temp = temp_int + 0.0625f * temp_frac;
     }
 
     // Reset interrupt flag
@@ -337,6 +349,7 @@ uint16_t max30102_fifo_available(max30102_t *obj, uint8_t *wr, uint8_t *rd){
 uint16_t max30102_read_fifo(max30102_t *obj, max30102_record *record, uint16_t max_samples){
 	int16_t num_samples = 0;	/* Bien luu tong so mau trong FIFO */
 
+	/* Thuc hien doc con tro de tinh toan so samples hien co */
 #ifdef DEBUG_SWV_ITM
 	int16_t actual_samples = 0;
 	uint8_t rd_ptr_before = 0;
@@ -366,10 +379,14 @@ uint16_t max30102_read_fifo(max30102_t *obj, max30102_record *record, uint16_t m
 			MAXLOWLEVELCHECKFUNC(max30102_read(obj, MAX30102_FIFO_DATA, data_temp, bytesToRead));
 
 #ifdef DEBUG_SWV_ITM
-			/* Doc lai rd_ptr sau khi I2C read xong FIFO de biet thuc su da doc duoc bao nhieu */
+			/* Doc lai rd_ptr sau khi I2C read xong FIFO de biet thuc su da doc duoc bao nhieu (theo doi con tro doc)*/
 			uint8_t rd_ptr_after = max30102_get_read_pointer(obj);
 
-			/* Tinh so samples thuc su da doc duoc */
+			/**
+			 * So con tro doc truoc va sau khi doc FIFO de xem con tro doc dich bao nhieu tinh so samples thuc su da doc duoc.
+			 * Binh thuong, neu doc du 32 samples, con tro doc se quay lai 1 vong ve vi tri cu. Nhung neu co ly do nao do
+			 * khien con tro doc bi lech (vd: rd_ptr_after lai be hon rd_ptr_before hoac bi ket khien no khong quay het vong FIFO)
+			 */
 			actual_samples = ((int16_t)(rd_ptr_after - rd_ptr_before) & 0x1F);
 			if(actual_samples == 0) actual_samples = 32;
 			if(actual_samples > max_samples) actual_samples = max_samples;
@@ -380,26 +397,6 @@ uint16_t max30102_read_fifo(max30102_t *obj, max30102_record *record, uint16_t m
 			SWV_LOG("[DEBUG] wr_ptr_before=%u, rd_ptr_before=%u, rd_ptr_after=%u, ovf=%u, calc=%d, actual=%d\r\n",
 					wr_ptr_before, rd_ptr_before, rd_ptr_after, ovf, num_samples, actual_samples);
 #endif // DEBUG_SWV_ITM
-
-			/**
-			 * DEBUG NOTE:
-			 * He thong chay muọt, binh thuong nhung dot nhien con tro doc nhay loan:
-			 * [DEBUG] wr_ptr_before=26, rd_ptr_before=25, rd_ptr_after=26, ovf=0, calc=1, actual=1
-			 * [DEBUG] wr_ptr_before=25, rd_ptr_before=26, rd_ptr_after=25, ovf=0, calc=31, actual=31
-			 * [DEBUG] wr_ptr_before=25, rd_ptr_before=25, rd_ptr_after=30, ovf=0, calc=32, actual=5
-			 * [DEBUG] wr_ptr_before=25, rd_ptr_before=30, rd_ptr_after=25, ovf=0, calc=27, actual=27
-			 * -> Kha nang phan cung bi Clock drift cho MCU dung clock tu thach anh con MAX dung internall oscillator (RC)
-			 * nen co sai so lon. O tan so 1000Hz (1ms/sample), su lech pha giua 2 clock tich luy dan sau moi chu ky 32ms
-			 * Cu sau 1 thoi gian, pha cua MCU va cam bien trung khit (Race Condition). Cam bien nap mau moi de len len
-			 * con tro ngay luc MCU chuan bi doc I2C, gay va cham va nhay loan xa con tro.
-			 * -> Ket qua: Phan cung bao hut mau dot ngot (tut xuong num_sample = 1, sau do lai vot len 27 de xa bo dem)
-			 *
-			 * Giai quyet: Software Padding samples
-			 * - Tuan thu phan cung: I2C chi doc dung so byte chip dang bao (khong ep/ghi de thanh ghi bay ba)
-			 * - Cuu luong Sync: Ep vong lap giai ma luon chay du 32 samples. Neu phan cung bi hut do lech pha
-			 * phan mem se tu dong kich hoat co che Padding (sao chep mau lien truoc vao cho trong)
-			 * - Ham luon return 32 de giu block size co dinh, triet tieu hoan toan loi Drop sample
-			 */
 
 			// Chuyen tung mau raw data thanh mau IR va RED
 			for(int8_t i = 0; i < MAX30102_SAMPLE_LEN_MAX; i++){
@@ -431,6 +428,7 @@ uint16_t max30102_read_fifo(max30102_t *obj, max30102_record *record, uint16_t m
 		}
 	}
 	return MAX30102_SAMPLE_LEN_MAX; //Tra ve so mau da doc
+	/* Note: Must read the FIFO_samples_debuglog.txt to see how hard bug is TT */
 }
 
 /*-----------------------------------------------------------*/
